2463386189378f70fdc3eabf9ad9b8b0b8618d1b
Grayden Smith <grayden@tech-vision.ca>
2014-12-24 14:30:11 +0200

    Initial commit, homepage starting to come together

---
+++ resources/templates/welcome.blade.php
@@ -0,0 +1,69 @@
+<html>
+	<head>
+        <link href="css/app.css" rel="stylesheet" />
+    </head>
+	<body>
+		<div class="container-fluid">
+            <div class="row">
+                <div class="col-sm-12" id="header-row">
+                    <div class="pull-left">
+                        <img src="http://www.gravatar.com/avatar/94d37519144d33b62997a1f8fba7accf?s=300" class="img-circle"/>
+                    </div>
+                    <div class="pull-left">
+                        <div id="header-message">
+                            <h1>Grayden Smith</h1>
+                            I'm Grayden and welcome to my home page. I wrote this up to show some of my favorite open source projects that I've worked on in three major languages. Check out the list below.
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-sm-mini"></div>
+                <div class="col-sm-3 project-column" id="ruby-column">
+                    <div class="text-center">
+                        <div class="language-title">Ruby</div>
+                    </div>
+                    <div class="project-list">
+                        <div class="project">
+                            <div class="project-name">Forekast</div>
+                            <div class="project-summary">Check out <a href="https://www.forekast.com">www.forekast.com</a>. Forekast is a site that's all aobut events, and a community of people that comment on and vote for the best upcoming events out there. The project is written in Ruby on Rails with a fair amount of javascript. Check out the project here: <a href="https://github.com/forekasting/kiwi.git">github.com/forekasting/kiwi</a>.</div>
+                        </div>
+                        <div class="project">
+                            <div class="project-name">Mark Version</div>
+                            <div class="project-summary"></div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm-1"></div>
+                <div class="col-sm-3 project-column" id="php-column">
+                    <div class="text-center">
+                        <div class="language-title">PHP</div>
+                    </div>
+                    <div class="project-list">
+                        <div class="project">
+                            <div class="project-name">Save Up</div>
+                            <div class="project-summary"></div>
+                        </div>
+                        <div class="project">
+                            <div class="project-name">Moss n Geckos</div>
+                            <div class="project-summary"></div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm-1"></div>
+                <div class="col-sm-3 project-column" id="javascript-column">
+                    <div class="text-center">
+                        <div class="language-title">Javascript</div>
+                    </div>
+                    <div class="project-list">
+                        <div class="project">
+                            <div class="project-name">Image Trimmer</div>
+                            <div class="project-summary"></div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm-mini"></div>
+            </div>
+        </div>
+	</body>
+</html>
